d3497c1bfafe8a71bd051ab0e1f3a79ee6fe6d36
ahmerique <ahmouckerick@gmail.Com>
2018-10-18 11:16:30 +0200

    add post request

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,20 @@ var express = require('express');
 const HttpStatus = require('http-status-codes');
 var router = express.Router();
 
-//GET some list
-router.get('/search', function(req, res, next) {
+//GET search bar
+router.get('/', function(req, res, next) {
   var words = req.query.words;
   var list_words=words.split('|')
   
   var list_id={'id': list_words}
   res.status(HttpStatus.OK).send(list_id);  
 });
+
+//POST entry form 
+router.post('/', function(req, res, next) {
+  console.log("element1 "+req.body.element1)
+  res.status(HttpStatus.OK).send(req.body);  
+});
+
 
 module.exports = router;
